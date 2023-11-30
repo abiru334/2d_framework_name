3f07d31af3fbced0bb82336e7cfb6158d8a909c1
S0523 <S0523@21S0523>
2023-11-30 09:48:48 +0900

    aaaaaaaaa

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+	//	kadaiB();
+	}
+
+		static void kadaiA() {
+	for(int b = 1;b < 11;b++) {
+		System.out.println("現在"+ b + "回目のループです");
+	}
+	
+}
+}
